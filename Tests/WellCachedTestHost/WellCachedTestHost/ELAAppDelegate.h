b9888f3e578d70e873bd77df533ee13de751cf2e
Eric Allam <eallam@icloud.com>
2014-03-24 14:04:47 +0000

    Without a test host, the test target started crashing

---
+++ Tests/WellCachedTestHost/WellCachedTestHost/ELAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ELAAppDelegate.h
+//  WellCachedTestHost
+//
+//  Created by Eric Allam on 24/03/2014.
+//  Copyright (c) 2014 Eric Allam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tests/WellCachedTestHost/WellCachedTestHost/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WellCachedTestHost
+//
+//  Created by Eric Allam on 24/03/2014.
+//  Copyright (c) 2014 Eric Allam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ELAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ELAAppDelegate class]));
+    }
+}
